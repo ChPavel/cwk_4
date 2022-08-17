cb8a90cd282fd9ca56f4db8af51aa726006821b6
Павел <chernovpp@gmail.com>
2022-08-17 23:47:50 +0700

    тупик!

--- project/models.py
+++ project/models.py
@@ -1,9 +1,64 @@
-from sqlalchemy import Column, String
-
+from sqlalchemy import Column, String, Integer, Float, ForeignKey
+from sqlalchemy.orm import relationship
 from project.setup.db import models
 
 
 class Genre(models.Base):
     __tablename__ = 'genres'
+    __tableargs__ = {'comments': 'Таблица жанров'}
 
-    name = Column(String(100), unique=True, nullable=False)
+    name = Column(String(100), unique=True, nullable=False, comment="Название")
+
+    def __repr__(self):
+        return f'{self.__tableargs__["comments"]}: ' \
+               f'{self.id}, {self.name}'
+
+class Director(models.Base):
+    __tablename__ = 'directors'
+    __tableargs__ = {'comments': 'Таблица режиссёров'}
+
+    name = Column(String(100), unique=True, nullable=False, comment="Имя")
+
+    def __repr__(self):
+        return f'{self.__tableargs__["comments"]}: ' \
+               f'{self.id}, {self.name}'
+
+
+class User(models.Base):
+    __tablename__ = 'users'
+    __tableargs__ = {'comments': 'Таблица пользователей'}
+
+    email = Column(String(100), nullable=False, comment="электронная почта")
+    password = Column(String(100), nullable=False, comment="пароль")
+    name = Column(String(100), comment="Имя")
+    surname = Column(String(100), comment="Фамилия")
+    favorite_genre = Column(String(100), comment="Любимый жанр")
+
+    def __repr__(self):
+        return f'{self.__tableargs__["comments"]}: ' \
+               f'{self.id}, {self.name}, {self.surname},' \
+               f'{self.email}, ' \
+               f'{self.password}, ' \
+               f'{self.favorite_genre}'
+
+
+class Movie(models.Base):
+    __tablename__ = 'movies'
+    __tableargs__ = {'comments': 'Таблица кинофильмов'}
+
+    title = Column(String(255), nullable=False, comment="Название")
+    description = Column(String(255), nullable=False, comment="Описание")
+    trailer = Column(String(255), nullable=False, comment="Промо-ролик")
+    year = Column(Integer, nullable=False, comment="Год создания")
+    rating = Column(Float, nullable=False, comment="Рейтинг")
+    genre_id = Column(Integer, ForeignKey("genres.id"), nullable=False)
+    genres = relationship("Genre")
+    director_id = Column(Integer, ForeignKey("directors.id"), nullable=False)
+    directors = relationship("Director")
+
+    def __repr__(self):
+        return f'{self.__tableargs__["comments"]}: ' \
+               f'{self.id}, {self.rating}, {self.year},' \
+               f'{self.title}, {self.trailer}' \
+               f'{self.directors}, ' \
+               f'{self.genre_id}, {self.director_id}'
--- project/setup/api/models.py
+++ project/setup/api/models.py
@@ -2,7 +2,36 @@ from flask_restx import fields, Model
 
 from project.setup.api import api
 
+
 genre: Model = api.model('Жанр', {
     'id': fields.Integer(required=True, example=1),
     'name': fields.String(required=True, max_length=100, example='Комедия'),
+})
+
+
+director: Model = api.model('Режиссёр', {
+    'id': fields.Integer(required=True, example=1),
+    'name': fields.String(required=True, max_length=100, example='Тейлор Шеридан'),
+})
+
+
+user: Model = api.model('Пользователь', {
+    'id': fields.Integer(required=True, example=1),
+    'email': fields.String(required=True, max_length=100, example='smitt@gmail.com'),
+    'password': fields.String(required=True, max_length=100, example='Zxc123/*cv'),
+    'name': fields.String(required=True, max_length=100, example='Sergey'),
+    'surname': fields.String(required=True, max_length=100, example='Sergeev'),
+    'favorite_genre': fields.String(required=True, max_length=100, example='Комедия'),
+})
+
+
+movie: Model = api.model('Кинофильм', {
+    'id': fields.Integer(required=True, example=1),
+    'title': fields.String(required=True, max_length=255, example='Омерзительная восьмерка'),
+    'description': fields.String(required=True, max_length=100, example='США после Гражданской войны'),
+    'trailer': fields.String(required=True, max_length=100, example='https://www.youtube.com/watch?v=lmB9VWm0okU'),
+    'year': fields.String(required=True, example=2015),
+    'rating': fields.Float(required=True, example=7.8),
+    'genre_id': fields.Integer(required=True, example=1),
+    'director_id': fields.Integer(required=True, example=1)
 })
